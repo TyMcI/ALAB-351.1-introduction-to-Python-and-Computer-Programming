82ff0066b7ab111c415ff1ed5bdf42a363002028
TyMcI <177349430+TyMcI@users.noreply.github.com>
2026-07-06 20:13:24 -0400

    added part1

---
+++ input.py
@@ -0,0 +1,4 @@
+print("\n\nHello, World!")
+print("Welcome to Python programming.")
+name = input("What is your name? ")
+print(f"\n\nHello, {name}! Glad to have you learning Python.\n\n")
---
+++ types_and_vars.py
@@ -0,0 +1,14 @@
+# 
+name = "Ty"
+age = 25
+heightInFeet = 5.7
+height = heightInFeet * 0.3048
+
+print("\n\nHello, my name is", name, ". I am ", age, " years old and ", height," meters tall.")
+print() 
+
+ #  
+ # After the introduction sentence, add code that calculates 
+ # what your age will be in 5 years and print a sentence stating that. 
+ # For example: "In 5 years, I will be 25 years old."
+ #    
