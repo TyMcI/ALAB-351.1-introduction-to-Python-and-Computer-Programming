dc008b33fa7479faaaa4dce940a5668c323c570a
TyMcI <177349430+TyMcI@users.noreply.github.com>
2026-07-08 21:04:57 -0400

    Updated Practice-section3.py

---
+++ Practice-section3.py
@@ -0,0 +1,25 @@
+n = 0
+
+while n!=3:
+    print (n)
+    n+=1
+
+else:
+    print (n,"else")
+
+print()
+
+for 1 in range(0,3):
+    print()
+else
+    print(1, "else")
+
+
+print (not(x>3 and x<10))
+print()
+
+
+# Code left shifyt and right shif efrom notes
+Var =17
+var_right = Var>> 1
+var_left = Var << 2
