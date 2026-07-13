f01af1c80df8e5818df44519ddd1e70fbc55a40b
TyMcI <177349430+TyMcI@users.noreply.github.com>
2026-07-13 01:04:04 -0400

    Staged Sentinel.py

--- Sentinel.py
+++ Sentinel.py
@@ -8,21 +8,26 @@
 # Hint: You will need to track two things: the user's current guess and a counter variable that starts at 0 and goes up by 1 every time they guess wrong. Use the and or or logical operators to combine your loop conditions.
 
 Invalid_Attempts=3
-Attemps=1
+Attempts=1
+INVALID_pwd = False
 
 n=0
-while Attempts != Invalid_Attempts:
+while not INVALID_pwd:
     PASSWORD = input("Please enter your password: ")
-    if ((PASSWORD != "python123") and (Attempts == 3))
-        Attempts = Attemps + 1
-        print("Access Denies")
-        Break:
-    else:
-        print("Invalide attemp, Please try again.")
+    if (PASSWORD == "python123"):
+        INVALID_pwd = True
+        print("Access Granted!!") 
+    else:   
+        if (Attempts <3):
+            Attempts = Attempts + 1
+            print("   Invalid Password. Try again>")
+            continue
+        else:
+            print("   Account locked. Try again later.")
+            break
+       
         
-     
-     
-print("Access Granted")
+
 
     
 
